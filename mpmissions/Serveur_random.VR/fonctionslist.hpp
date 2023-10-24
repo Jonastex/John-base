0a49dd3559408c528b851136f62bd823efb9a978
Jonastex <30275981+Jonastex@users.noreply.github.com>
2023-10-25 01:13:47 +0200

    Update fonctionslist.hpp

--- mpmissions/Serveur_random.VR/fonctionslist.hpp
+++ mpmissions/Serveur_random.VR/fonctionslist.hpp
@@ -1,12 +1,3 @@
-/*
- * Filename: d:\Serveur_random\steamapps\common\Arma 3 Server\mpmissions\Serveur_random.VR\fonctionslist.hpp
- * Path: d:\Serveur_random\steamapps\common\Arma 3 Server\mpmissions\Serveur_random.VR
- * Created Date: Tuesday, February 22nd 2022, 6:55:09 pm
- * Author: Jonastex
- * 
- * Copyright (c) 2022 John
- */
-
 class Client_loadinter {
 	tag = "john";
 	class function {
